cd9f981079a9f84d0494fbe9d3aa77ba5357abce
JNocetti <juan.anocetti@gmail.com>
2025-06-13 19:22:24 -0300

    errors again

--- src/components/navbars/MainNavBar.tsx
+++ src/components/navbars/MainNavBar.tsx
@@ -7,7 +7,7 @@ const MainNavbar = () => {
             <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
                 <div className="flex items-center space-x-2">
                     <img
-                        src='/mainIcon.png'
+                        src='mainIcon.png'
                         alt="Logo"
                         className="h-10 w-auto object-contain"
                     />
